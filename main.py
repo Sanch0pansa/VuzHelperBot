e6fd68745ac9b39498f3bfc724f2eb9e10595228
Лебедев Александр <god@lifegame.su>
2023-09-29 22:00:57 +0300

    feat: add USerController to main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from classes import Bot
-from controllers.WelcomeController import WelcomeController
+from controllers.UserController import UserController
 import os
 from dotenv import load_dotenv
 
-wc = WelcomeController()
+uc = UserController()
 
 load_dotenv()
 bot = Bot.Bot(os.environ.get('TOKEN'))
-bot.add_controllers([wc])
+bot.add_controllers([uc])
 bot.polling()
